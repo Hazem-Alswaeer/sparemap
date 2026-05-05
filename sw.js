db0edb8411b6923882762a1864b313b231103cd4
Hazem-Alswaeer <111688440+Hazem-Alswaeer@users.noreply.github.com>
2026-05-05 23:27:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'sparemap-v1';
-const ASSETS = ['/', '/index.html', '/db.js'];
+const ASSETS = ['/sparemap/', '/sparemap/index.html', '/sparemap/db.js', '/sparemap/sw.js', '/sparemap/manifest.json'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
   self.skipWaiting();
